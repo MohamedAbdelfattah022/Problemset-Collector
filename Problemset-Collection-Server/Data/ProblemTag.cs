8cfbda25a2f5506385269001ca084271104e6ac9
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2024-09-26 19:37:52 +0300

    ProblemTags CRUD 'Done'

--- Problemset-Collection-Server/Data/ProblemTag.cs
+++ Problemset-Collection-Server/Data/ProblemTag.cs
@@ -4,7 +4,6 @@ namespace Problemset_Collection_Server.Data
 {
     public class ProblemTag
     {
-        [Key]
         public int TagId {  get; set; }
         public int ProblemId {  get; set; }
     }
